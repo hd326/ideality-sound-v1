3f0b1ef9697d4115148a2440e98787042d656331
hd326 <31417369+hd326@users.noreply.github.com>
2019-12-16 10:30:30 -0800

    update css

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -14,15 +14,16 @@
             <div class="post-card">
                 <a href="{{ url('/'.$post->slug) }}">
                     <div class="image-image-container">
-                            <div class="image-container">
-                                <div class="image-container-helper"></div><img src="{{ asset('images/yulongstack.JPG') }}">
-                            </div>
+                        <div class="image-container">
+                            <div class="image-container-helper"></div><img src="{{ asset('images/yulongstack.JPG') }}">
                         </div>
+                    </div>
                 </a>
                 <h2><a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a></h2>
                 <p><i class="fas fa-tags"></i><span>@foreach($post->tags as
                         $tag){{ $loop->first ? '' : ', ' }}<a title="View all posts in {{ $tag->name }}"
-                            href="/tags/{{$tag->name}}">{{ $tag->name }}</a>@endforeach</span><br><i class="fas fa-calendar"></i>
+                            href="/tags/{{$tag->name}}">{{ $tag->name }}</a>@endforeach</span><br><i
+                        class="fas fa-calendar"></i>
                     {{ $post->created_at->format('F j, Y') }}</p>
                 <p>{{ substr(strip_tags($post->body), 0, 125) }} {{ strlen($post->body) > 125 ? "..." : "" }}</p>
                 <hr>
--- resources/views/pages/subcategory.blade.php
+++ resources/views/pages/subcategory.blade.php
@@ -11,15 +11,16 @@
             <div class="post-card">
                 <a href="{{ url('/'.$post->slug) }}">
                     <div class="image-image-container">
-                            <div class="image-container">
-                                <div class="image-container-helper"></div><img src="{{ asset('images/yulongstack.JPG') }}">
-                            </div>
+                        <div class="image-container">
+                            <div class="image-container-helper"></div><img src="{{ asset('images/yulongstack.JPG') }}">
                         </div>
+                    </div>
                 </a>
                 <h2><a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a></h2>
                 <p><i class="fas fa-tags"></i>
                     @foreach($post->tags as
-            $tag){{ $loop->first ? '' : ', ' }}<a title="View all posts in {{ $tag->name }}" href="/tags/{{$tag->name}}">{{ $tag->name }}</a>@endforeach
+                    $tag){{ $loop->first ? '' : ', ' }}<a title="View all posts in {{ $tag->name }}"
+                        href="/tags/{{$tag->name}}">{{ $tag->name }}</a>@endforeach
                     <br><i class="fas fa-calendar"></i>
                     {{ $post->created_at->format('F j, Y') }}</p>
                 <p>{{ substr(strip_tags($post->body), 0, 125) }} {{ strlen($post->body) > 125 ? "..." : "" }}</p>
